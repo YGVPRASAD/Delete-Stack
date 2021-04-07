78f4a001f6949d175deabcb5d18a3e49407b7a87
YGVPRASAD <55577916+YGVPRASAD@users.noreply.github.com>
2021-04-07 18:31:51 +0530

    Create delete-stack.py

---
+++ delete-stack.py
@@ -0,0 +1,48 @@
+##delete a stack with given stack name##
+from __future__ import division, print_function, unicode_literals
+from os import environ
+import logging
+import argparse
+import boto3
+
+LOGGER = logging.getLogger(__name__)
+LOGFORMAT = "%(levelname)s: %(message)s"
+LOGGER = logging.getLogger("Launch RDS CFT")
+LOGLEVEL = environ.get("logLevel", "INFO")
+logging.basicConfig(format=LOGFORMAT, level=LOGLEVEL)
+LOGGER.setLevel(logging.getLevelName(LOGLEVEL))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', type=str, required=True)
+args = parser.parse_args()
+
+
+client = boto3.client('cloudformation')
+
+
+def main():
+    ###delete stack###
+    stack_name = args.name
+    params = {
+        'StackName': stack_name,
+    }
+    if _stack_exists(stack_name):
+        LOGGER.info("Deleting {}".format(stack_name))
+        response = client.delete_stack(
+            StackName=args.name
+        )
+    else:
+        LOGGER.info("Stack does not exist")
+        raise
+
+def _stack_exists(stack_name):
+    stacks = client.list_stacks()['StackSummaries']
+    for stack in stacks:
+        if stack['StackStatus'] == 'DELETE_COMPLETE':
+            continue
+        if stack_name == stack['StackName']:
+            return True
+    return False
+
+if __name__ == '__main__':
+    main()
