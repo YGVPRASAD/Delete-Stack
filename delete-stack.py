fa11607a479420da429c15dbddaad74141106c5f
YGVPRASAD <55577916+YGVPRASAD@users.noreply.github.com>
2021-04-08 19:23:57 +0530

    Update delete-stack.py

--- delete-stack.py
+++ delete-stack.py
@@ -44,7 +44,8 @@ def main():
         except ClientError as error:
             LOGGER.info("Deleted {}".format(stack_name))  
     else:
-        LOGGER.info("Stack does not exist")
+        raise
+        #LOGGER.info("Stack does not exist")
     
 def _stack_exists(stack_name):
     stacks = client.list_stacks()['StackSummaries']
